ace4b9e604df43205211747bd513ce2575ff72ab
mathew512 <mathewkasnga59@gmail.com>
2025-12-18 11:44:24 +0300

    Add ProgrammingSection.java for Java programming tasks

---
+++ ProgrammingSection.java
@@ -0,0 +1,6 @@
+public class ProgrammingSection {
+    // This class is for Java programming tasks
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Programming Section!");
+    }
+}
